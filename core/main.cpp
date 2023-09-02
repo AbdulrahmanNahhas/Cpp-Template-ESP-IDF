dd87a79ff6deb0bec3447e260abcfa251ac119a9
Abdulrahman Nahhas <91866837+AbdulrahmanNahhas@users.noreply.github.com>
2023-09-02 14:10:15 +0300

    Update main.cpp

--- core/main.cpp
+++ core/main.cpp
@@ -1,13 +1,7 @@
 #include "main.h"
-#include "esp_log.h"
-
-// Define the logging level and tag.
-#define LOG_LEVEL_LOCAL ESP_LOG_VERBOSE
-#define LOG_TAG "MAIN"
 
 // Create a static instance of the Main class.
 static Main main_app;
-
 extern "C" void app_main(void) {
   ESP_ERROR_CHECK(main_app.setup()); // Initialize the Main object.
   while (true) {
@@ -17,17 +11,11 @@ extern "C" void app_main(void) {
 
 esp_err_t Main::setup(void) {
   esp_err_t status {ESP_OK};
-  status |= led.init();
 
   return status;
 }
 
 void Main::loop(void) {
-  ESP_LOGI("LED STATUS", "ON");
-  led.setState(true);
-  vTaskDelay(pdSecond);
-
-  ESP_LOGI("LED STATUS", "OFF");
-  led.setState(true);
+  printf("hello world!");
   vTaskDelay(pdSecond);
 }
